d5cf9a5edcc48e5d729dc199cf74ee7b964a244b
Tuti Wulandari <tutiwulandari150@gmail.com>
2021-02-23 00:36:50 +0700

    Menambahkan bottomNav

--- app/src/main/java/com/example/shoppinglist_navigation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist_navigation/MainActivity.kt
@@ -2,15 +2,7 @@ package com.example.shoppinglist_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,40 +13,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
+//        supportActionBar?.hide()
 
-//        navHostFragment =
-//                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        navController = navHostFragment.findNavController()
-                switchFragment(addItemFragment)
-             bottom_navigation.setOnNavigationItemReselectedListener {
+//        switchFragment(addItemFragment)
+        bottom_navigation.setOnNavigationItemReselectedListener {
             when(it.itemId) {
-                R.id.navigation_create -> switchFragment(addItemFragment)
-                R.id.navigationList -> switchFragment(listItemFragment)
+                R.id.navigation_create -> {
+                    switchFragment(addItemFragment)
+                    replaceTitle(getString(R.string.title_create))
+                }
+                R.id.navigation_list -> {
+                    switchFragment(listItemFragment)
+                    replaceTitle(getString(R.string.title_list))
+                }
             }
             true
         }
     }
 
-
-
-//        val buttonAddItemMenu = findViewById<Button>(R.id.btn_create)
-//        buttonAddItemMenu.setOnClickListener{
-//            println("BTN ADD")
-//            findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_addItemFragment)
-//        }
-//        val buttonViewListMenu = findViewById<Button>(R.id.btn_list)
-//        buttonViewListMenu.setOnClickListener{
-//            findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_listItemFragment)
-//        }
-
-
     private fun switchFragment(fragment: Fragment) {
-//        if (fragment != null) {
-//            val transaction = supportFragmentManager.beginTransaction()
-//                    transaction.replace(R.id.nav_host_fragment, fragment).transaction.commit()
-//        }
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, fragment)
+            transaction.commit()
+        }
     }
-
-
+    private fun  replaceTitle(title:String) {
+        supportActionBar?.title = title
+    }
 }
